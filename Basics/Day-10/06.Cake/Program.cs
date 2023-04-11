38455e5a164f09081ed9d0e370cb412ed6ebcce6
Stela Kazandzhieva <stela.kazandzhieva@gmail.com>
2023-04-11 16:44:40 +0300

    11.04.16:44

---
+++ Basics/Day-10/06.Cake/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int pieceOfCake = width * length;
+            int takenPieces = 0;
+            string action = Console.ReadLine();
+
+            while (action != "STOP" && pieceOfCake > takenPieces)
+            {
+                int currentPieces = int.Parse(action);
+                takenPieces += currentPieces;
+                action = Console.ReadLine();
+              
+            }
+            if (takenPieces >= pieceOfCake)
+            {
+                Console.WriteLine($"No more cake left! You need {takenPieces - pieceOfCake} pieces more.");
+            }
+            if (action == "STOP")
+            {
+                Console.WriteLine($"{pieceOfCake - takenPieces} pieces are left.");
+            }
+
+        }
+    }
+}
+
---
+++ Basics/Day-10/07.Moving/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+            int h = int.Parse(Console.ReadLine());
+
+            int newHomeSpace = w * l * h;
+            int TotalBaggage = 0;
+            string command = Console.ReadLine();
+
+            while ((command != "Done") && (newHomeSpace > TotalBaggage))
+            {
+                int cartons = int.Parse(command);
+                TotalBaggage += cartons;
+                command = Console.ReadLine();
+            }
+            if (command != "Done")
+            {
+                Console.WriteLine($"{newHomeSpace - TotalBaggage} Cubic meters left.");
+            }
+            if (newHomeSpace < TotalBaggage)
+            {
+                Console.WriteLine($"No more free space! You need {TotalBaggage - newHomeSpace} Cubic meters more.");
+            }
+        }
+    }
+}
+
